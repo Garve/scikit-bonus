9611f92681bd243a4b59e328e8e0cfd49d47822d
Robert <xgarve@gmail.com>
2021-01-31 22:08:56 +0100

    readme and setup.py

---
+++ README.md
@@ -0,0 +1,2 @@
+# scikit-bonus
+This is a package with scikit-learn-compatible transformers, classifiers and regressors that I find useful.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scikit-bonus-garve",  # Replace with your own username
+    version="0.0.1",
+    author="Robert Kübler",
+    author_email="xgarve@gmail.com",
+    description="Extending scikit-learn with useful things",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Garve/scikit-bonus",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
