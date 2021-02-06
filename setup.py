9145a7a6a3377533e3ec80da4ce8348f4884b9df
Robert <xgarve@gmail.com>
2021-02-06 21:50:28 +0100

    bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="scikit-bonus",
-    version="0.0.3",
+    version="0.0.4",
     author="Robert Kübler",
     author_email="xgarve@gmail.com",
     description="Extending scikit-learn with useful things",
