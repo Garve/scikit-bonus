94059aff3bd870d9ff21ddbc8ad09368c645c68d
Robert <xgarve@gmail.com>
2021-02-13 22:03:32 +0100

    changed docstrings

--- skbonus/exceptions.py
+++ skbonus/exceptions.py
@@ -1,6 +1,9 @@
+"""Special Exceptions."""
+
+
 class NoFrequencyError(Exception):
     """
     This exception is thrown when a DatetimeIndex does not have a frequency.
-    This can happen if two disjoint DatetimeIndex objects are combined via a union, for example when dealing
-    with cross validation.
+
+    This can happen if two disjoint DatetimeIndex objects are combined via a union, for example when dealing with cross validation.
     """
