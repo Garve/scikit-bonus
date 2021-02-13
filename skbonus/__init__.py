8be6659ddcb8dc0043fd3d9bb4e82ef4eea42fa2
Robert <xgarve@gmail.com>
2021-02-13 22:13:58 +0100

    changed docstrings

--- skbonus/__init__.py
+++ skbonus/__init__.py
@@ -1,1 +1,6 @@
+"""This module contains classes for an easy workflow with pandas and scikit-learn.
+
+Usually methods take dataframes as an input and return dataframes again.
+"""
+
 __version__ = "0.0.8.1"
