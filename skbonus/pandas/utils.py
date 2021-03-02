e3654f53f2552bfa2fdc8e79881e16316ee8ce50
Garve <xgarve@gmail.com>
2021-03-02 11:20:37 +0100

    doc strings

--- skbonus/pandas/utils.py
+++ skbonus/pandas/utils.py
@@ -1,14 +1,32 @@
 """Deal with dataframes."""
 
+from typing import Any
+
 import pandas as pd
 
 
-def make_df_output(estimator):
-    class EstimatorWithDataFrameOutput(estimator):
-        def transform(self, X):
+def make_df_output(estimator: Any) -> Any:
+    """
+    Make a scikit-learn transformer output pandas dataframes, if its inputs are dataframes.
+
+    Parameters
+    ----------
+    estimator : scikit-learn transformer
+        Some transformer with a `transform` method.
+
+    Returns
+    -------
+    scikit-learn transformer
+        Transformer with an altered `transform` method that outputs a dataframe with the same columns and index as the input X.
+
+    """
+
+    class TransformerWithDataFrameOutput(estimator):
+        def transform(self, X: pd.DataFrame) -> pd.DataFrame:
             cols = X.columns
             index = X.index
             out = super().transform(X)
+
             return pd.DataFrame(out, columns=cols, index=index)
 
-    return EstimatorWithDataFrameOutput
+    return TransformerWithDataFrameOutput
