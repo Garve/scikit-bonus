528a6e01ca2e71cfa3f78b1e3769833dff2be81d
Robert <xgarve@gmail.com>
2021-03-07 17:34:59 +0100

    error handling, prefit estimators

--- skbonus/meta/_zero_inflated_regressor.py
+++ skbonus/meta/_zero_inflated_regressor.py
@@ -3,7 +3,14 @@ from __future__ import annotations
 from typing import Any
 
 import numpy as np
-from sklearn.base import BaseEstimator, RegressorMixin, clone
+from sklearn.base import (
+    BaseEstimator,
+    RegressorMixin,
+    clone,
+    is_regressor,
+    is_classifier,
+)
+from sklearn.exceptions import NotFittedError
 from sklearn.utils.validation import check_is_fitted, check_X_y, check_array
 
 
@@ -70,20 +77,43 @@ class ZeroInflatedRegressor(BaseEstimator, RegressorMixin):
         -------
         ZeroInflatedRegressor
             Fitted regressor.
+
+        Raises
+        ------
+        ValueError
+            If the estimator passed as `classifier` (`regressor`) is not a classifier (regressor).
         """
         X, y = check_X_y(X, y)
         self._check_n_features(X, reset=True)
+        if not is_classifier(self.classifier):
+            raise ValueError(
+                f"`classifier` has to be a classifier. Received instance of {type(self.classifier)} instead."
+            )
+        if not is_regressor(self.regressor):
+            raise ValueError(
+                f"`regressor` has to be a regressor. Received instance of {type(self.regressor)} instead."
+            )
 
-        self.classifier_ = clone(self.classifier)
-        self.classifier_.fit(X, y != 0)
+        try:
+            check_is_fitted(self.classifier)
+            self.classifier_ = self.classifier
+        except NotFittedError:
+            self.classifier_ = clone(self.classifier)
+            self.classifier_.fit(X, y != 0)
 
         non_zero_indices = np.where(self.classifier_.predict(X) == 1)[0]
 
         if non_zero_indices.size > 0:
-            self.regressor_ = clone(self.regressor)
-            self.regressor_.fit(X[non_zero_indices], y[non_zero_indices])
+            try:
+                check_is_fitted(self.regressor)
+                self.regressor_ = self.regressor
+            except NotFittedError:
+                self.regressor_ = clone(self.regressor)
+                self.regressor_.fit(X[non_zero_indices], y[non_zero_indices])
         else:
-            self.regressor_ = None
+            raise ValueError(
+                "The predicted training labels are all zero, making the regressor obsolete. Change the classifier or use a plain regressor instead."
+            )
 
         return self
 
@@ -108,7 +138,7 @@ class ZeroInflatedRegressor(BaseEstimator, RegressorMixin):
         output = np.zeros(len(X))
         non_zero_indices = np.where(self.classifier_.predict(X))[0]
 
-        if self.regressor_ is not None and non_zero_indices.size > 0:
+        if non_zero_indices.size > 0:
             output[non_zero_indices] = self.regressor_.predict(X[non_zero_indices])
 
         return output
