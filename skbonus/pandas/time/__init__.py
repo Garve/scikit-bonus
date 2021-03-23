3300427e7ada4c03937b4714dd1bc3033d1c1fff
Garve <xgarve@gmail.com>
2021-03-23 16:24:02 +0100

    import error

--- skbonus/pandas/time/__init__.py
+++ skbonus/pandas/time/__init__.py
@@ -1,12 +1,10 @@
 """Module for time series utilities with a focus on pandas compatibility."""
 
-from ._continuous import PowerTrend, GeneralGaussianSmoother, ExponentialDecaySmoother
+from ._continuous import PowerTrend
 from ._simple import SimpleTimeFeatures, DateIndicator
 
 __all__ = [
     "PowerTrend",
-    "GeneralGaussianSmoother",
-    "ExponentialDecaySmoother",
     "SimpleTimeFeatures",
     "DateIndicator",
 ]
